169748144fa9205aaec5b849398bd7ec8fc74cef
ngerek <ngere.k@outlook.com>
2023-11-13 21:08:43 +0100

    created route for Cart and Signin components

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import About from "./pages/about/About";
 import Menu from "./pages/menu/Menu";
 import Gallery from "./pages/gallery/Gallery";
 import Reservation from "./pages/reservation/Reservation";
+import Cart from "./pages/cart/Cart";
+import Signin from "./pages/signin/Signin";
 import Dishes from "./pages/dishes/Dishes";
 import DishesInfo from "./pages/dishesInfo/DishesInfo";
-
+import Navbar from "./components/navbar/Navbar";
 import Error from "./pages/error/Error";
 
 // styles
@@ -17,12 +19,15 @@ import "./App.scss";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="menu" element={<Menu />} />
         <Route path="gallery" element={<Gallery />} />
         <Route path="reservation" element={<Reservation />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="sign-in" element={<Signin />} />
         <Route path="dishes" element={<Dishes />} />
         <Route path="dishes/:id" element={<DishesInfo />} />
         <Route path="*" element={<Error />} />
