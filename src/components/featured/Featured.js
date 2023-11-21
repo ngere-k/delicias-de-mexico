b80620b1b7da3a758706fe31f005aabebac406ca
ngerek <ngere.k@outlook.com>
2023-11-21 21:02:00 +0100

    built and styled Featured component

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,27 @@
+import microsoft from "../../assets/img/logos/microsoft.png";
+import google from "../../assets/img/logos/google.png";
+import meta from "../../assets/img/logos/meta.png";
+import yahoo from "../../assets/img/logos/yahoo.png";
+
+// styles
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <section className="section-featured">
+      <div className="container">
+        <h2 className="heading-secondary featured-text-color text-center">
+          Trusted by thousands of companies
+        </h2>
+        <div className="logos">
+          <img src={microsoft} alt="microsoft logo" />
+          <img src={google} alt="google logo" />
+          <img src={meta} alt="meta logo" />
+          <img src={yahoo} alt="yahoo logo" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
