7ebe807ca5f7e8a5ba3f8ebe4d68c4a7c8fa4d9a
ngerek <ngere.k@outlook.com>
2023-11-25 13:47:14 +0100

    connect app with redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./sass/index.scss";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
