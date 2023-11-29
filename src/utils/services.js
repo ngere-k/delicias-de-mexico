6b7bd9c3b8dafff9b9a28c8bff70e012bfb299a4
ngerek <ngere.k@outlook.com>
2023-11-29 14:03:14 +0100

    created services array in utils

---
+++ src/utils/services.js
@@ -0,0 +1,32 @@
+import { PiBowlFoodFill, PiWifiHighFill, PiTruckFill } from "react-icons/pi";
+import { FaGrinTongue } from "react-icons/fa";
+
+export const services = [
+  {
+    id: 1,
+    icon: <PiBowlFoodFill />,
+    title: "quality food",
+    text: "We offer quality food at a reasonable price, with a variety of dishes to suit every taste and preference",
+  },
+
+  {
+    id: 2,
+    icon: <FaGrinTongue />,
+    title: "super taste",
+    text: "Super taste that is unmatched by any other, with dishes that are flavorful, aromatic, and mouth-watering",
+  },
+
+  {
+    id: 3,
+    icon: <PiWifiHighFill />,
+    title: "free wifi",
+    text: "We provide free wifi, so our customers can stay connected and entertained while enjoying their meals",
+  },
+
+  {
+    id: 4,
+    icon: <PiTruckFill />,
+    title: "fast delivery",
+    text: "We guarantee fast delivery of your orders, so you can enjoy your food while it is hot and fresh",
+  },
+];
