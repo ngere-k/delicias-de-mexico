fec871f157013b894d8fbdf3bba5256cb684b570
ngerek <ngere.k@outlook.com>
2023-12-03 16:09:45 +0100

    included reviewsReducer in redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import foodsReducer from "./features/foods/foodsSlice";
+import reviewsReducer from "./features/reviews/reviewsSlice";
 
 export const store = configureStore({
   reducer: {
     foods: foodsReducer,
+    reviews: reviewsReducer,
   },
 });
