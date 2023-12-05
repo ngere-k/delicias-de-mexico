1314dc37a6011debef0a26498faf0d235dac7858
ngerek <ngere.k@outlook.com>
2023-12-05 16:09:15 +0100

    refactor HomeTestimonials

--- src/components/homeTestimonials/HomeTestimonials.js
+++ src/components/homeTestimonials/HomeTestimonials.js
@@ -1,4 +1,7 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { PiArrowRight } from "react-icons/pi";
+import CustomerReview from "../customerReview/CustomerReview";
 
 // styles
 import "./HomeTestimonials.scss";
@@ -6,12 +9,37 @@ import "./HomeTestimonials.scss";
 const HomeTestimonials = () => {
   const { reviews, isLoading, error } = useSelector((store) => store.reviews);
 
+  const reviewsSlice = reviews?.slice(0, 3);
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <section className="section-home-testimonials">
       <div className="container">
         <div>
           <span className="subheading">testimonials</span>
           <h2 className="heading-secondary">Don't just take our word for it</h2>
+        </div>
+
+        <div className="reviews">
+          {reviewsSlice.map((review) => {
+            return <CustomerReview key={review.id} {...review} />;
+          })}
+        </div>
+
+        <div className="read-more">
+          <Link className="read-more__link">
+            <button className="read-more__btn">
+              Read more customer reviews
+            </button>
+            <PiArrowRight className="read-more__icon" />
+          </Link>
         </div>
       </div>
     </section>
