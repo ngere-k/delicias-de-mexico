ed709214c30adc58eb5a31042c87fb0de3be5f1d
ngerek <ngere.k@outlook.com>
2023-12-05 16:05:48 +0100

    built CustomerReview

---
+++ src/components/customerReview/CustomerReview.js
@@ -0,0 +1,29 @@
+import { PiStarFill } from "react-icons/pi";
+
+// styles
+import "./CustomerReview.scss";
+
+const CustomerReview = ({ id, name, text, image }) => {
+  return (
+    <div className="review" key={id}>
+      <div className="review__img-box">
+        <img className="review__img" src={image} alt={name} />
+      </div>
+
+      <div>
+        <p className="review__name">{name}</p>
+        <div className="review__stars">
+          <PiStarFill />
+          <PiStarFill />
+          <PiStarFill />
+          <PiStarFill />
+          <PiStarFill />
+        </div>
+      </div>
+
+      <p className="review__text">{text}</p>
+    </div>
+  );
+};
+
+export default CustomerReview;
