28a1ccd3a9e217267a8271350cf6781441900393
ngerek <ngere.k@outlook.com>
2023-12-07 22:21:00 +0100

    applied Link to Logo component

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,15 +1,18 @@
 import logo from "../../assets/img/logo.png";
+import { Link } from "react-router-dom";
 
 // styles
 import "./Logo.scss";
 
 const Logo = ({ medium }) => {
   return (
-    <img
-      className={medium ? "medium" : "logo"}
-      src={logo}
-      alt="delicias de mexico logo"
-    />
+    <Link to="/">
+      <img
+        className={medium ? "medium" : "logo"}
+        src={logo}
+        alt="delicias de mexico logo"
+      />
+    </Link>
   );
 };
 
