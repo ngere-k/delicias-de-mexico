f9d544a0275fbda0d175394f349f2005dedd2078
ngerek <ngere.k@outlook.com>
2023-12-07 22:29:58 +0100

    created footerNavLinks in utils.js

---
+++ src/utils/footerNavLinks.js
@@ -0,0 +1,60 @@
+export const footerNavLinks = {
+  company: [
+    {
+      id: 1,
+      name: "About",
+      path: "/about",
+    },
+    {
+      id: 2,
+      name: "Contact",
+      path: "/contact",
+    },
+    {
+      id: 3,
+      name: "Team",
+      path: "/team",
+    },
+    {
+      id: 4,
+      name: "Careers",
+      path: "/careers",
+    },
+  ],
+
+  "useful-links": [
+    {
+      id: 1,
+      name: "Menu",
+      path: "/menu",
+    },
+    {
+      id: 2,
+      name: "Gallery",
+      path: "/gallery",
+    },
+    {
+      id: 3,
+      name: "Reservation",
+      path: "/reservation",
+    },
+    {
+      id: 4,
+      name: "Order online",
+      path: "/dishes",
+    },
+  ],
+
+  account: [
+    {
+      id: 1,
+      name: "Create account",
+      path: "/sign-in",
+    },
+    {
+      id: 2,
+      name: "Sign in",
+      path: "/sign-in",
+    },
+  ],
+};
