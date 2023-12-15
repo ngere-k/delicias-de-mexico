3787d17a1619ebf1be525993b480a3772c4db9e9
ngerek <ngere.k@outlook.com>
2023-12-15 19:27:52 +0100

    imported modalReducer into store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import foodsReducer from "./features/foods/foodsSlice";
 import reviewsReducer from "./features/reviews/reviewsSlice";
+import modalReducer from "./features/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     foods: foodsReducer,
     reviews: reviewsReducer,
+    modal: modalReducer,
   },
 });
