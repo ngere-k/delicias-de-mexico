b19b07623e01b55d66afdc2b9314e701e617c538
ngerek <ngere.k@outlook.com>
2023-12-19 21:05:47 +0100

    implement footer form

--- src/components/footer/FooterForm.js
+++ src/components/footer/FooterForm.js
@@ -1,8 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const FooterForm = () => {
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!email) {
+      toast.error("Please fill in the email field!");
+      return;
+    }
+
+    toast.success("You have successfully subscribe to our newsletter!");
+    setEmail("");
   };
 
   return (
@@ -12,8 +23,10 @@ const FooterForm = () => {
       <form className="footer__form" onSubmit={handleSubmit}>
         <input
           type="email"
+          value={email}
           className="footer__input"
           placeholder="Your email here..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button type="submit" className="footer__sign-up">
           Sign up
