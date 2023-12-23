7ec4ad1990ddf3cc69bf6908cdaa7e6d38bf1c20
ngerek <ngere.k@outlook.com>
2023-12-23 23:22:46 +0100

    built sign in page

--- src/pages/signin/Signin.js
+++ src/pages/signin/Signin.js
@@ -4,7 +4,38 @@ import React from "react";
 import "./Signin.scss";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <section className="sign-in">
+      <form className="form">
+        <div>
+          <h2 className="heading-secondary">Sign in</h2>
+        </div>
+
+        <div className="form__content">
+          <label className="form__box">
+            <span className="form__name">Name</span>
+            <input type="text" className="form__input" />
+          </label>
+
+          <label className="form__box">
+            <span className="form__name">Email</span>
+            <input type="email" className="form__input" />
+          </label>
+
+          <label className="form__box">
+            <span className="form__name">Password</span>
+            <input type="password" className="form__input" />
+          </label>
+        </div>
+
+        <div className="form__btn-box">
+          <button type="submit" className="btn">
+            Sign in
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Signin;
