82a3ea783cfeb984ed044959db06bc97f06f3f39
ngerek <ngere.k@outlook.com>
2024-01-10 23:19:47 +0100

    created contacts array

---
+++ src/utils/contacts.js
@@ -0,0 +1,24 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { PiPhoneFill } from "react-icons/pi";
+import { MdEmail } from "react-icons/md";
+
+export const contacts = [
+  {
+    id: 1,
+    name: "address",
+    icon: <IoLocationSharp />,
+    text: "12 Adeola Odeku Street, Victoria Island, Lagos.",
+  },
+  {
+    id: 2,
+    name: "phone",
+    icon: <PiPhoneFill />,
+    text: "08060086558",
+  },
+  {
+    id: 3,
+    name: "email",
+    icon: <MdEmail />,
+    text: "support@deliciasdemexico.com",
+  },
+];
