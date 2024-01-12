8bdf0dd2b161d29a32b4f92b4b1c7c6c2ab0f577
ngerek <ngere.k@outlook.com>
2024-01-12 21:31:34 +0100

    added handleChange and value props

--- src/components/inputRow/InputRow.js
+++ src/components/inputRow/InputRow.js
@@ -1,13 +1,15 @@
 // styles
 import "./InputRow.scss";
 
-const InputRow = ({ type, placeholder, name }) => {
+const InputRow = ({ type, placeholder, name, handleChange, value }) => {
   return (
     <label className="input__box">
       <span className="input__name">{name}</span>
       <input
         type={type}
         className="input__field"
+        value={value}
+        onChange={handleChange}
         placeholder={placeholder}
         required
       />
