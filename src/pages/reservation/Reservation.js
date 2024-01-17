c94404e76493105a2b108efd89886548b5cd5de8
ngerek <ngere.k@outlook.com>
2024-01-17 12:22:49 +0100

    implemented form

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import InputRow from "../../components/inputRow/InputRow";
 import InputSelect from "../../components/inputSelect/InputSelect";
 import PageHero from "../../components/pageHero/PageHero";
 import { people } from "../../utils/people";
-import { time } from "../../utils/time";
+import { times } from "../../utils/times";
+import { toast } from "react-toastify";
 
 // styles
 import "./Reservation.scss";
 
 const Reservation = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [numPeople, setNumPeople] = useState("1 person");
+  const [phone, setPhone] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("10:00 am");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.success("Your reservation is confirmed.");
+
+    setName("");
+    setEmail("");
+    setDate("");
+    setNumPeople("1 person");
+    setPhone("");
+    setDate("");
+    setTime("10:00 am");
+  };
+
   return (
     <article className="reservation">
       <PageHero
@@ -15,13 +37,55 @@ const Reservation = () => {
         heading=" Book your spot for a delicious Mexican breakfast"
       />
 
-      <form className="container reservation__form">
-        <InputRow type="text" name="name" placeholder="Your full name" />
-        <InputRow type="email" name="email" placeholder="Your email address" />
-        <InputRow type="tel" name="phone" placeholder="Your phone" />
-        <InputSelect label="we are" name="people" array={people} />
-        <InputRow type="date" name="date" />
-        <InputSelect label="time" name="time" array={time} />
+      <form className="container reservation__form" onSubmit={handleSubmit}>
+        {/* name */}
+        <InputRow
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Your full name"
+          handleChange={(e) => setName(e.target.value)}
+        />
+        {/* email */}
+        <InputRow
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Your email address"
+          handleChange={(e) => setEmail(e.target.value)}
+        />
+        {/* phone */}
+        <InputRow
+          type="tel"
+          name="phone"
+          value={phone}
+          placeholder="Your phone"
+          handleChange={(e) => setPhone(e.target.value)}
+        />
+        {/* number of people */}
+        <InputSelect
+          label="we are"
+          name="people"
+          array={people}
+          value={numPeople}
+          handleChange={(e) => setNumPeople(e.target.value)}
+        />
+        {/* date */}
+        <InputRow
+          type="date"
+          name="date"
+          value={date}
+          handleChange={(e) => setDate(e.target.value)}
+        />
+        {/* time */}
+        <InputSelect
+          label="time"
+          name="time"
+          array={times}
+          value={time}
+          handleChange={(e) => setTime(e.target.value)}
+        />
+
         <div className="reservation__btn-box">
           <button type="submit" className="btn btn--block">
             Reserve a table
