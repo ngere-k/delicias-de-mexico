aae6eb98f2476d95ed6769263fb7d7ac572d8d05
ngerek <ngere.k@outlook.com>
2024-01-17 12:22:06 +0100

    added value and onChange prop

--- src/components/inputSelect/InputSelect.js
+++ src/components/inputSelect/InputSelect.js
@@ -1,12 +1,17 @@
 // styles
 import "./InputSelect.scss";
 
-const InputSelect = ({ label, array, SelectName }) => {
+const InputSelect = ({ label, array, SelectName, value, handleChange }) => {
   return (
     <label className="select__box">
       <span className="select__label">{label}</span>
 
-      <select name={SelectName} className="select__field">
+      <select
+        name={SelectName}
+        className="select__field"
+        value={value}
+        onChange={handleChange}
+      >
         {array.map((ele) => {
           return (
             <option key={ele} value={ele}>
