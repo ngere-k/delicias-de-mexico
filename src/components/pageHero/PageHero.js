fd531482ad759019f2386aaa7337abf705e2b736
ngerek <ngere.k@outlook.com>
2024-01-19 19:45:24 +0100

    included heroClass prop

--- src/components/pageHero/PageHero.js
+++ src/components/pageHero/PageHero.js
@@ -3,9 +3,9 @@ import React from "react";
 // styles
 import "./PageHero.scss";
 
-const PageHero = ({ subheading, heading }) => {
+const PageHero = ({ subheading, heading, heroClass }) => {
   return (
-    <div className="page-hero reservation__hero">
+    <div className={`page-hero ${heroClass}`}>
       <div className="page-hero__content">
         <span className="subheading font-size-sm">{subheading}</span>
         <h2 className="page-hero__heading">{heading}</h2>
