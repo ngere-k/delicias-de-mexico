3110dde659f382bab8a63b8192c051adf6db5346
ngerek <ngere.k@outlook.com>
2024-01-24 17:41:35 +0100

    imported userReducer

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import foodsReducer from "./features/foods/foodsSlice";
 import reviewsReducer from "./features/reviews/reviewsSlice";
 import modalReducer from "./features/modal/modalSlice";
+import userReducer from "./features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     foods: foodsReducer,
     reviews: reviewsReducer,
     modal: modalReducer,
+    user: userReducer,
   },
 });
