d2a27289347e6f2878218e7e57d7527a17a90f97
ngerek <ngere.k@outlook.com>
2024-01-27 22:45:33 +0100

    built Gallery.js

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,10 +1,37 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 // styles
 import "./Gallery.scss";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { foods, isLoading, error } = useSelector((store) => store.foods);
+  const foodsSlice = foods.slice(15, 28);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
+  return (
+    <article className="gallery">
+      <div className="gallery__content">
+        {foodsSlice.map((food, index) => {
+          const { id, title, image } = food;
+          return (
+            <figure
+              key={id}
+              className={`gallery__item gallery__item--${index + 1}`}
+            >
+              <img src={image} alt={title} className="gallery__image" />
+            </figure>
+          );
+        })}
+      </div>
+    </article>
+  );
 };
 
 export default Gallery;
