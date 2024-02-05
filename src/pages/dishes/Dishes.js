3f114a98a80cbd5b2877037ff1ae9bcef6428d4e
ngerek <ngere.k@outlook.com>
2024-02-05 22:58:29 +0100

    built Dishes.js

--- src/pages/dishes/Dishes.js
+++ src/pages/dishes/Dishes.js
@@ -1,10 +1,54 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import PageHero from "../../components/pageHero/PageHero";
+import { PiCurrencyNgn } from "react-icons/pi";
 
 // styles
 import "./Dishes.scss";
 
 const Dishes = () => {
-  return <div>Dishes</div>;
+  const { foods, isLoading, error } = useSelector((store) => store.foods);
+  const foodsSlice = foods.slice(94, 109);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
+  }
+
+  return (
+    <article className="dishes">
+      <div className="container">
+        <ul className="dishes__items">
+          {foodsSlice.map((food) => {
+            return (
+              <li key={food.id} className="dishes__list">
+                <figure className="dishes__figure">
+                  <img
+                    src={food.image}
+                    alt={food.title}
+                    className="dishes__img"
+                  />
+                </figure>
+
+                <div className="dishes__content">
+                  <h4 className="dishes__heading">{food.title}</h4>
+
+                  <div className="price__container">
+                    <div>
+                      <PiCurrencyNgn className="price__icon" />
+                    </div>
+                    <p className="price__amount">10,000.00</p>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </article>
+  );
 };
 
 export default Dishes;
