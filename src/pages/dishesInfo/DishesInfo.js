d1ef4b44a28101d617c9528c95f6290b97b6414b
ngerek <ngere.k@outlook.com>
2024-02-08 18:33:14 +0100

    imported fetchSingleFood action

--- src/pages/dishesInfo/DishesInfo.js
+++ src/pages/dishesInfo/DishesInfo.js
@@ -1,10 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchSingleFood } from "../../features/foods/foodsSlice";
 
 // styles
 import "./DishesInfo.scss";
 
 const DishesInfo = () => {
-  return <div>DishesInfo</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchSingleFood(id));
+  }, [dispatch, id]);
+
+  return <article className="single-food">{id}</article>;
 };
 
 export default DishesInfo;
