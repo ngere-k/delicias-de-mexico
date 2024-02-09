1c356b23d4c7dbf7933c94e30ad96117ac4a9322
ngerek <ngere.k@outlook.com>
2024-02-09 22:45:47 +0100

    api custom error message

--- src/features/foods/foodsSlice.js
+++ src/features/foods/foodsSlice.js
@@ -24,6 +24,11 @@ export const fetchFoods = createAsyncThunk(
       return res.data;
     } catch (error) {
       console.log(error.response);
+      if (error.response.status === 429) {
+        return rejectWithValue(
+          "You have exceeded the MONTHLY quota for Requests on your current plan, BASIC. Please check back on 20th."
+        );
+      }
       return rejectWithValue(error.response.data.message);
     }
   }
@@ -46,6 +51,11 @@ export const fetchSingleFood = createAsyncThunk(
       return res.data;
     } catch (error) {
       console.log(error.response);
+      if (error.response.status === 429) {
+        return rejectWithValue(
+          "You have exceeded the MONTHLY quota for Requests on your current plan, BASIC. Please check back on 20th."
+        );
+      }
       return rejectWithValue(error.response.data.message);
     }
   }
