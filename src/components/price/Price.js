4c29df2d39a8d1832ded4905d17a662637f09065
ngerek <ngere.k@outlook.com>
2024-02-13 21:19:05 +0100

    added big prop & rendered class conditionally

--- src/components/price/Price.js
+++ src/components/price/Price.js
@@ -3,13 +3,13 @@ import { PiCurrencyNgn } from "react-icons/pi";
 // styles
 import "./Price.scss";
 
-const Price = ({ amount }) => {
+const Price = ({ amount, big }) => {
   return (
     <div className="price__container">
       <div>
         <PiCurrencyNgn className="price__icon" />
       </div>
-      <p className="price__amount">{amount}</p>
+      <p className={big ? "price__amount--big" : "price__amount"}>{amount}</p>
     </div>
   );
 };
