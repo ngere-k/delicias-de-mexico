84a51afb41f46b607f33e168adad91f835f8f915
ngerek <ngere.k@outlook.com>
2024-03-03 15:18:06 +0100

    addToCart reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,7 +10,42 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, { payload }) => {
+      const {
+        food: { id, image, title },
+        price,
+        stock,
+        amount,
+      } = payload;
+
+      const findCartItem = state.cart.find((item) => item.id === id);
+
+      if (findCartItem) {
+        state.cart = state.cart.map((cartItem) => {
+          if (cartItem.id === id) {
+            let newCartAmount = cartItem.amount + 1;
+            if (newCartAmount > stock) {
+              newCartAmount = stock;
+            }
+            return newCartAmount;
+          } else {
+            return cartItem;
+          }
+        });
+      } else {
+        const newCartItem = {
+          id,
+          image,
+          title,
+          price,
+          stock,
+          amount,
+        };
+        state.cart.push(newCartItem);
+      }
+    },
+  },
 });
 
 export const { addToCart } = cartSlice.actions;
