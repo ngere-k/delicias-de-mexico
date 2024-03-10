009236991122b32e8da6846c2ed54933ded3b295
ngerek <ngere.k@outlook.com>
2024-03-10 23:02:03 +0100

    build CartContent.js

--- src/components/cartContent/CartContent.js
+++ src/components/cartContent/CartContent.js
@@ -1,10 +1,36 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+import Price from "../price/Price";
+import AmountButtons from "../amountButtons/AmountButtons";
 
 // styles
 import "./CartContent.scss";
 
 const CartContent = () => {
-  return <div>CartContent</div>;
+  const { cart } = useSelector((store) => store.cart);
+
+  return (
+    <div className="cart__content">
+      {cart.map((item) => {
+        const { id, image, title, amount, price, stock } = item;
+        return (
+          <div key={id} className="cart__product">
+            <div className="cart__figure-container">
+              <figure className="cart__figure">
+                <img src={image} alt={title} className="cart__img" />
+              </figure>
+              <figcaption className="cart__figcaption">{title}</figcaption>
+            </div>
+            <AmountButtons amount={amount} />
+            <Price amount={price * amount} />
+            <button className="cart__btn">
+              <MdDelete className="cart__delete-icon" />
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CartContent;
