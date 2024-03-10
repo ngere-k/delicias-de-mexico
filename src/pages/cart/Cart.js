ec63c70f69e70850348e7feab9dc525bd820399c
ngerek <ngere.k@outlook.com>
2024-03-10 22:57:18 +0100

    built Cart.js

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,10 +1,26 @@
-import React from "react";
+import CartHeader from "../../components/cartHeader/CartHeader";
+import CartColumn from "../../components/cartColumn/CartColumn";
+import CartContent from "../../components/cartContent/CartContent";
+import CartSummary from "../../components/cartSummary/CartSummary";
 
 // styles
 import "./Cart.scss";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <article className="article-cart">
+      <div className="cart container">
+        <div className="cart__container">
+          <CartHeader />
+          <div>
+            <CartColumn />
+            <CartContent />
+          </div>
+        </div>
+        <CartSummary />
+      </div>
+    </article>
+  );
 };
 
 export default Cart;
