fc36594e0455292d98194f5348f4fec29e2b608b
ngerek <ngere.k@outlook.com>
2024-03-12 17:34:27 +0100

    useSelector to access cart store & used localStorage api in useEffect

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import CartHeader from "../../components/cartHeader/CartHeader";
 import CartColumn from "../../components/cartColumn/CartColumn";
 import CartContent from "../../components/cartContent/CartContent";
 import CartSummary from "../../components/cartSummary/CartSummary";
+import { useSelector } from "react-redux";
 
 // styles
 import "./Cart.scss";
 
 const Cart = () => {
+  const { cart } = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    localStorage.setItem("foodCart", JSON.stringify(cart));
+  }, [cart]);
+
   return (
     <article className="article-cart">
       <div className="cart container">
@@ -14,7 +22,11 @@ const Cart = () => {
           <CartHeader />
           <div>
             <CartColumn />
-            <CartContent />
+            <div className="cart__content">
+              {cart.map((item) => {
+                return <CartContent key={item.id} {...item} />;
+              })}
+            </div>
           </div>
         </div>
         <CartSummary />
