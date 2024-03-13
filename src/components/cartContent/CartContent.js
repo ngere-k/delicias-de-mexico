1aee2f9d92ca9dc62128b2bafd8475d6917b212c
ngerek <ngere.k@outlook.com>
2024-03-13 15:00:11 +0100

    built CartContent functionality

--- src/components/cartContent/CartContent.js
+++ src/components/cartContent/CartContent.js
@@ -1,34 +1,43 @@
-import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { MdDelete } from "react-icons/md";
-import Price from "../price/Price";
 import AmountButtons from "../amountButtons/AmountButtons";
+import {
+  removeCartItem,
+  toggleCartAmount,
+} from "../../features/cart/cartSlice";
+import { formatPrice } from "../../utils/formatPrice";
 
 // styles
 import "./CartContent.scss";
 
-const CartContent = () => {
-  const { cart } = useSelector((store) => store.cart);
+const CartContent = ({ id, image, title, amount, price }) => {
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch(toggleCartAmount({ id, name: "increase" }));
+  };
+
+  const decrease = () => {
+    dispatch(toggleCartAmount({ id, name: "decrease" }));
+  };
 
   return (
-    <div className="cart__content">
-      {cart.map((item) => {
-        const { id, image, title, amount, price, stock } = item;
-        return (
-          <div key={id} className="cart__product">
-            <div className="cart__figure-container">
-              <figure className="cart__figure">
-                <img src={image} alt={title} className="cart__img" />
-              </figure>
-              <figcaption className="cart__figcaption">{title}</figcaption>
-            </div>
-            <AmountButtons amount={amount} />
-            <Price amount={price * amount} />
-            <button className="cart__btn">
-              <MdDelete className="cart__delete-icon" />
-            </button>
-          </div>
-        );
-      })}
+    <div className="cart__product">
+      <div className="cart__figure-container">
+        <figure className="cart__figure">
+          <img src={image} alt={title} className="cart__img" />
+        </figure>
+        <figcaption className="cart__figcaption">{title}</figcaption>
+      </div>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <p className="price--sm text-bold-500">{formatPrice(price * amount)}</p>
+
+      <button
+        className="cart__btn"
+        onClick={() => dispatch(removeCartItem(id))}
+      >
+        <MdDelete className="cart__delete-icon" />
+      </button>
     </div>
   );
 };
