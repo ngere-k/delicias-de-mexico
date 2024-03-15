b4375134827d6bdc60b23f51380939deffa88642
ngerek <ngere.k@outlook.com>
2024-03-15 23:18:55 +0100

    used total_items variable from cart store in cart__item span

--- src/components/cartHeader/CartHeader.js
+++ src/components/cartHeader/CartHeader.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import { PiArrowLeft } from "react-icons/pi";
 import { MdDelete } from "react-icons/md";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { clearCart } from "../../features/cart/cartSlice";
 
 // styles
 import "./CartHeader.scss";
 
 const CartHeader = () => {
+  const { total_items: items } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
 
   return (
@@ -21,7 +22,10 @@ const CartHeader = () => {
       <div className="cart__rule" />
       <div className="cart__name">
         <h2 className="cart__heading">
-          Cart <span className="cart__item">(3 items in your cart)</span>
+          Cart
+          <span className="cart__item">{`(${
+            items > 1 ? `${items} items` : `${items} item`
+          } in your cart)`}</span>
         </h2>
 
         <button className="cart__delete" onClick={() => dispatch(clearCart())}>
