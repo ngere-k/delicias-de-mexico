c1764fd8adbf007f47a8eb504e6564eac739eb72
ngerek <ngere.k@outlook.com>
2024-03-16 23:40:14 +0100

    dispatch cartTotals & conditional render empty cart

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,19 +1,36 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import CartHeader from "../../components/cartHeader/CartHeader";
 import CartColumn from "../../components/cartColumn/CartColumn";
 import CartContent from "../../components/cartContent/CartContent";
 import CartSummary from "../../components/cartSummary/CartSummary";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { cartTotals } from "../../features/cart/cartSlice";
 
 // styles
 import "./Cart.scss";
 
 const Cart = () => {
   const { cart } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(cartTotals());
     localStorage.setItem("foodCart", JSON.stringify(cart));
   }, [cart]);
+
+  if (cart.length < 1) {
+    return (
+      <article className="empty">
+        <h2 className="empty__heading">
+          Empty cart? Let’s change that with flavor!
+        </h2>
+        <Link to="/dishes" className="btn">
+          Start shopping
+        </Link>
+      </article>
+    );
+  }
 
   return (
     <article className="article-cart">
