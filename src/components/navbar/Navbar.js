77aa37c9b18b0d1cfd0cd4214f4d9843fb6eb44c
ngerek <ngere.k@outlook.com>
2024-04-15 21:10:25 +0100

    mobile navigation implementation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,16 +6,26 @@ import {
   PiShoppingCartFill,
 } from "react-icons/pi";
 import { useSelector, useDispatch } from "react-redux";
+import { signOutUser } from "../../features/user/userSlice";
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import { closeSidebar, openSidebar } from "../../features/modal/modalSlice";
 
 // styles
 import "./Navbar.scss";
-import { signOutUser } from "../../features/user/userSlice";
 
 const Navbar = () => {
   const { user } = useSelector((store) => store.user);
+  const { isSidebarOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(signOutUser());
+    dispatch(closeSidebar());
+  };
+
   return (
-    <header className="header">
+    <header className={isSidebarOpen ? "header sidebar-open" : "header"}>
       <Logo />
 
       <nav className="main-nav">
@@ -25,47 +35,57 @@ const Navbar = () => {
               to="/"
               className={`main-nav__link ${({ isActive }) =>
                 isActive ? "active" : ""}`}
+              onClick={() => dispatch(closeSidebar())}
             >
               Home
             </NavLink>
           </li>
-
           <li>
-            <NavLink to="/about" className="main-nav__link">
+            <NavLink
+              to="/about"
+              className="main-nav__link"
+              onClick={() => dispatch(closeSidebar())}
+            >
               About
             </NavLink>
           </li>
-
           <li>
-            <NavLink to="/menu" className="main-nav__link">
+            <NavLink
+              to="/menu"
+              className="main-nav__link"
+              onClick={() => dispatch(closeSidebar())}
+            >
               Menu
             </NavLink>
           </li>
-
           <li>
-            <NavLink to="/gallery" className="main-nav__link">
+            <NavLink
+              to="/gallery"
+              className="main-nav__link"
+              onClick={() => dispatch(closeSidebar())}
+            >
               Gallery
             </NavLink>
           </li>
-
           <li>
-            <NavLink to="/reservation" className="main-nav__link">
+            <NavLink
+              to="/reservation"
+              className="main-nav__link"
+              onClick={() => dispatch(closeSidebar())}
+            >
               Reservation
             </NavLink>
           </li>
         </ul>
 
         <ul className="main-nav__list-2">
-          <li>
-            <span to="sign-in" className="main-nav__user">
-              {/* user name here */}
-              {/* Hi, Kanyun Ngere */}
-            </span>
-          </li>
-
           {!user && (
             <li>
-              <NavLink to="sign-in" className="main-nav__link">
+              <NavLink
+                to="sign-in"
+                className="main-nav__link"
+                onClick={() => dispatch(closeSidebar())}
+              >
                 <span className="main-nav__icon">
                   <PiUserPlusFill />
                 </span>
@@ -78,10 +98,7 @@ const Navbar = () => {
 
           {user && (
             <li>
-              <Link
-                className="main-nav__link"
-                onClick={() => dispatch(signOutUser())}
-              >
+              <Link className="main-nav__link" onClick={handleClick}>
                 <span className="main-nav__icon">
                   <PiUserMinusFill />
                 </span>
@@ -93,7 +110,11 @@ const Navbar = () => {
           )}
 
           <li className="main-nav__item">
-            <NavLink to="/cart" className="main-nav__link">
+            <NavLink
+              to="/cart"
+              className="main-nav__link"
+              onClick={() => dispatch(closeSidebar())}
+            >
               <span className="main-nav__icon">
                 <PiShoppingCartFill />
               </span>
@@ -102,7 +123,6 @@ const Navbar = () => {
               </span>
             </NavLink>
           </li>
-
           <li>
             <Link to="/dishes">
               <button className="btn">Order online</button>
@@ -110,6 +130,24 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
+
+      {!isSidebarOpen && (
+        <button
+          className="btn-mobile-nav"
+          onClick={() => dispatch(openSidebar())}
+        >
+          <IoMdMenu className="icon-mobile-nav" />
+        </button>
+      )}
+
+      {isSidebarOpen && (
+        <button
+          className="btn-mobile-nav btn-mobile-nav--close"
+          onClick={() => dispatch(closeSidebar())}
+        >
+          <IoMdClose className="icon-mobile-nav" />
+        </button>
+      )}
     </header>
   );
 };
